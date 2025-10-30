e35385b9e99897a57ea11b6bac85703ead32b34d
Corentin Talour <corentintalour@gmail.com>
2025-10-30 16:50:16 +0100

    Intégration EntraID et communication avec le back

--- front_gestionnaire_mot_de_passe/Program.cs
+++ front_gestionnaire_mot_de_passe/Program.cs
@@ -1,27 +1,39 @@
 using front_gestionnaire_mot_de_passe.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+string apiEndpoint = builder.Configuration.GetValue<string>("DownstreamApi:BaseUrl")
+                     ?? throw new InvalidOperationException("API BaseUrl missing");
+string apiScope = builder.Configuration.GetValue<string>("DownstreamApi:Scopes")
+                  ?? throw new InvalidOperationException("API Scope missing");
+
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi([apiScope])
+    .AddDownstreamApi("DownstreamApi", options =>
+    {
+        options.BaseUrl = apiEndpoint;
+        options.Scopes = [apiScope];
+    })
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization(o => o.FallbackPolicy = o.DefaultPolicy);
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+app.MapControllers();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
