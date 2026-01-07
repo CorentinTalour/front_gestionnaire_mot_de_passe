3615f51429c5ead678697793fc527175a8917fdb
CorentinTalour <azrod003@gmail.com>
2026-01-07 18:49:23 +0100

    Add logs functionality: create Log model, LogsModal component, and integrate with VaultPage

--- front_gestionnaire_mot_de_passe/Components/Pages/VaultPage.razor.cs
+++ front_gestionnaire_mot_de_passe/Components/Pages/VaultPage.razor.cs
@@ -1,10 +1,9 @@
-﻿using front_gestionnaire_mot_de_passe.Models;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Identity.Abstractions;
+﻿﻿using Microsoft.AspNetCore.Components;
 
 namespace front_gestionnaire_mot_de_passe.Components.Pages;
 
 public partial class VaultPage : ComponentBase
 {
-    private bool _isEditingMembers = false;
 }
+
+
---
+++ front_gestionnaire_mot_de_passe/Models/Log.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace front_gestionnaire_mot_de_passe.Models;
+
+public class Log
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public required string Url { get; set; }
+    
+    public float Duration { get; set; }
+    
+    public DateTime ExecutedAt { get; set; }
+    
+    public required string Message { get; set; }
+    
+    public int? EntraId { get; set; }
+
+
+    public int? VaultId { get; set; }
+    
+    
+}
